74d7a435f63157244514da0d12eb914ab2f6edf0
Edward Melendez Mendigure <192666@unsaac.edu.pe>
2022-10-26 00:30:40 -0500

    back end cronometro

--- js/cronometro.js
+++ js/cronometro.js
@@ -1,16 +1,45 @@
 export default function correrTiempo(inicio,reset,tiempoEjercicio,tiempoDescanzo,realTime){
     const d=document;
+    let stopWacthInterval;
+    let runtime=0
     d.addEventListener("click",e=>{
-        if(e.target.matches(inicio)){
-            const ejercicio=parseInt(d.getElementById(tiempoEjercicio).innerHTML),
+        const ejercicio=parseInt(d.getElementById(tiempoEjercicio).innerHTML),
                 descanzo=parseInt(d.getElementById(tiempoDescanzo).innerHTML);
-            const cuentaRegresiva=setInterval(() => {
-                let nuevo=ejercicio-1;
-                d.getElementById(realTime).innerHTML=nuevo;
-            }, 1000);
-            }
+        if(e.target.matches(inicio)){
             
+                let starTime=Date.now()-runtime;
+                stopWacthInterval=setInterval(() => {
+                    runtime=Date.now()-starTime;
+                    d.getElementById(realTime).innerHTML=Math.floor(runtime/1000)
 
-        })
 
-    }
+                if(Math.floor(runtime/1000)==ejercicio){
+                    runtime=descanzo+1  
+                    clearInterval(stopWacthInterval)
+                    let starTime=Date.now()-runtime;
+                    
+                    stopWacthInterval=setInterval(() => {
+                        runtime=Date.now()-starTime;
+                        d.getElementById(realTime).innerHTML=descanzo-Math.floor(runtime/1000);
+                        if(descanzo-Math.floor(runtime/1000)==0){
+                            console.log("entro");
+                            clearInterval(stopWacthInterval);
+                            runtime=0;
+                        }
+                        }, 1000);
+                    
+                    }
+                }, 1000);
+
+        }
+
+        if(e.target.matches(reset)){
+            clearInterval(stopWacthInterval);
+            d.getElementById(realTime).innerHTML=0;
+            runtime=0;
+        }
+        
+
+    })
+
+}
